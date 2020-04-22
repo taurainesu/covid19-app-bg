674755fa338ed1fea03fd34d6721d22e2c2a8467
Batsirai Gurure <batsiraigurure@yahoo.com>
2020-04-22 23:35:35 +0200

    Added Table page and Raw Data page with backend

--- app.js
+++ app.js
@@ -2,6 +2,11 @@
 var express = require('express');
 var app = express();
 
+
+//global variables to pass data from this script to the chartjs script
+var datesArray = [];
+var confirmedArray = [];
+
 //setup templating engine
 app.set('view engine', 'ejs');
 
@@ -27,9 +32,42 @@ app.get('/', function(req, res){
     };
     request(options, function (error, response) {
         var rawData=response.body;
-        var apiData=JSON.parse(response.body); 
+        apiData=JSON.parse(response.body); 
         if (error) throw new Error(error);     
-        res.render('index', {apiData, rawData});
-        console.log(typeof apiData);
+        res.render('index', {apiData, rawData, datesArray, confirmedArray});
     });
+});
+
+app.get('/table', function(req, res){
+   
+    var request = require('request');
+    var options = {
+        'method': 'GET',
+        'url': 'https://api.covid19api.com/live/country/zimbabwe/status/confirmed',
+        'headers': {
+        }
+    };
+    request(options, function (error, response) {
+        var rawData=response.body;
+        apiData=JSON.parse(response.body); 
+        if (error) throw new Error(error);     
+        res.render('table', {apiData, rawData, datesArray, confirmedArray});
+    }); 
+});
+
+app.get('/rawData', function(req, res){
+   
+    var request = require('request');
+    var options = {
+        'method': 'GET',
+        'url': 'https://api.covid19api.com/live/country/zimbabwe/status/confirmed',
+        'headers': {
+        }
+    };
+    request(options, function (error, response) {
+        var rawData=response.body;
+        apiData=JSON.parse(response.body); 
+        if (error) throw new Error(error);     
+        res.render('rawData', {apiData, rawData, datesArray, confirmedArray});
+    }); 
 });
